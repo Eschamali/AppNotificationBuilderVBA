fcdbdf80b3c215047c9ded7acc7f66fc603669f6
Eschamali <126876080+Eschamali@users.noreply.github.com>
2024-09-14 17:46:51 +0900

    トーストスケジュールの実装。
    UTC基準なので注意

--- AppNotificationBuilderVBA/AppNotificationBuilder.cpp
+++ AppNotificationBuilderVBA/AppNotificationBuilder.cpp
@@ -7,6 +7,22 @@ using namespace Windows::UI::Notifications;
 using namespace Windows::Data::Xml::Dom;
 
 
+// SYSTEMTIMEをDateTimeに変換する関数
+Windows::Foundation::DateTime SystemTimeToDateTime(const SYSTEMTIME& st) {
+    FILETIME fileTime;
+    SystemTimeToFileTime(&st, &fileTime);
+
+    // FILETIMEをLARGE_INTEGERに変換
+    ULARGE_INTEGER largeInt;
+    largeInt.LowPart = fileTime.dwLowDateTime;
+    largeInt.HighPart = fileTime.dwHighDateTime;
+
+    // FILETIMEの値を100ナノ秒単位で格納し、DateTimeに変換
+    Windows::Foundation::DateTime dateTime;
+    dateTime = winrt::clock::from_FILETIME(fileTime);
+    return dateTime;
+}
+
 void ShowToastNotification(ToastNotificationParams* params){
     // COMの初期化
     HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
@@ -28,65 +44,53 @@ void ShowToastNotification(ToastNotificationParams* params){
     double scheduleTime = params->scheduleTime;
 
     try {
-        // トースト通知の作成
-        ToastNotifier toastNotifier = ToastNotificationManager::CreateToastNotifier(appUserModelID);
+        // トースト通知のXMLを構築
         XmlDocument toastXml;
-        toastXml.LoadXml(xmlTemplate);  // XMLテンプレートをロード
+        toastXml.LoadXml(xmlTemplate);
 
-        // トースト通知オブジェクトを作成
-        ToastNotification toast{ toastXml };
 
-        // グループとタグを設定
-        toast.Group(group);
-        toast.Tag(tag);
-
-        //MessageBoxW(nullptr, appUserModelID, L"AppUserModelID", MB_OK);
-        //MessageBoxW(nullptr, xmlTemplate, L"XML Template", MB_OK);
-        //MessageBoxW(nullptr, group, L"Group", MB_OK);
-        //MessageBoxW(nullptr, tag, L"Tag", MB_OK);
-        wchar_t buffer[256];
-        swprintf(buffer, 256, L"ScheduleTime: %f", scheduleTime);
-        MessageBoxW(nullptr, buffer, L"Schedule Time", MB_OK);
-
+        // 通常の通知か、スケジュール通知かを分岐
+        ToastNotifier toastNotifier = ToastNotificationManager::CreateToastNotifier(appUserModelID);
         if (scheduleTime > 0) {
-            // スケジュール日時が指定されている場合
-            // VBAの日付型は1970年からの秒数で与えられるので、それを日時に変換する
-            FILETIME fileTime;
-            SYSTEMTIME systemTime;
-            LARGE_INTEGER largeInt;
-
-            // VBAから渡されるシリアル値（days since 1899-12-30）をWindows FILETIME形式に変換
-            double daysSinceEpoch = scheduleTime - 25569.0;  // 25569は1970-01-01からのシリアル値
-            long long total100Nanoseconds = static_cast<long long>(daysSinceEpoch * 864000000000.0);  // 1日=86400秒, 100ナノ秒=1秒/10^7
-
-            swprintf(buffer, 256, L"daysSinceEpoch: %f", daysSinceEpoch);
-            MessageBoxW(nullptr, buffer, L"Schedule Time", MB_OK);
-
-            largeInt.QuadPart = total100Nanoseconds;
-            fileTime.dwLowDateTime = largeInt.LowPart;
-            fileTime.dwHighDateTime = largeInt.HighPart;
-
-            // FILETIMEをSYSTEMTIMEに変換
-            FileTimeToSystemTime(&fileTime, &systemTime);
+            // スケジュール通知の場合
+            SYSTEMTIME st;
+            VariantTimeToSystemTime(scheduleTime, &st);
 
             // SYSTEMTIMEをDateTimeに変換
-            Windows::Foundation::DateTime scheduleDateTime;
-            scheduleDateTime = winrt::clock::from_FILETIME(fileTime);  // FILETIMEを使ってDateTimeに変換
+            Windows::Foundation::DateTime scheduleDateTime = SystemTimeToDateTime(st);
 
-            // ScheduledToastNotification を作成してスケジュール設定
-            ScheduledToastNotification scheduledToast{ toastXml, scheduleDateTime };
+            // time_since_epoch() から100ナノ秒単位の値を取得
+            auto duration = scheduleDateTime.time_since_epoch();
+            int64_t count = std::chrono::duration_cast<std::chrono::duration<int64_t, std::ratio<1, 10000000>>>(duration).count();
+
+            // スケジュールされたトースト通知を作成
+            ScheduledToastNotification scheduledToast(toastXml, scheduleDateTime);
+
+            // 上記で作成されたオブジェクトに各種設定(GroupとTag等)を施す
             scheduledToast.Group(group);
             scheduledToast.Tag(tag);
 
-            // 通知をスケジュール
+            // スケジュールトーストを追加
             toastNotifier.AddToSchedule(scheduledToast);
         }
         else {
-            // 即時通知を表示
+            // 通常のトースト通知を作成
+            ToastNotification toast(toastXml);
+
+            // 上記で作成されたオブジェクトに各種設定(GroupとTag等)を施す
+            toast.Group(group);
+            toast.Tag(tag);
+
+            // 通常の即時通知を作動
             toastNotifier.Show(toast);
         }
     }
     catch (const winrt::hresult_error& e) {
         MessageBoxW(nullptr, e.message().c_str(), L"エラー", MB_OK);
+    }
+
+    // CoUninitialize()は、CoInitializeExが成功した場合のみ呼び出す
+    if (SUCCEEDED(hr)) {
+        CoUninitialize();
     }
 }
