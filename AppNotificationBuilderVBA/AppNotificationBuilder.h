e91264cd03128634d5c97ab7643651c09f32e389
Eschamali <126876080+Eschamali@users.noreply.github.com>
2024-09-16 00:23:05 +0900

    値Check用のmsgboxを一旦実装
    ExpirationTime機能もひとまず準備

--- AppNotificationBuilderVBA/AppNotificationBuilder.h
+++ AppNotificationBuilderVBA/AppNotificationBuilder.h
@@ -12,18 +12,29 @@
 #define AppNotificationBuilderVBA_API __declspec(dllimport)
 #endif
 
-// 構造体を定義
-struct ToastNotificationParams {
+// 1つ目の構造体を定義。スタック領域の制限の都合上、複数の構造体を定義します。ここでは、文字列に関するパラメーターです
+// ※VBA側で、シグネチャ（型や順序）が合うようにすること。
+#pragma pack(4)
+struct ToastNotificationParams_String {
     const wchar_t* AppUserModelID;
     const wchar_t* XmlTemplate;
     const wchar_t* Tag;
     const wchar_t* Group;
-    bool ExpiresOnReboot;
-    bool SuppressPopup;
     const wchar_t* Schedule_ID;
-    double Schedule_DeliveryTime;
 };
 
+// 2つ目の構造体を定義。ここでは、スイッチングに関するパラメーターです
+struct ToastNotificationParams_Boolean {
+    BOOLEAN ExpiresOnReboot;
+    BOOLEAN SuppressPopup;
+};
+
+// 3つ目の構造体を定義。ここでは、日付に関するパラメーターです
+struct ToastNotificationParams_Date {
+    double Schedule_DeliveryTime;
+    double ExpirationTime;
+};
+#pragma pack()
 
 //関数宣言
-extern "C" AppNotificationBuilderVBA_API void ShowToastNotification(ToastNotificationParams* params);
+extern "C" AppNotificationBuilderVBA_API void __stdcall ShowToastNotification(ToastNotificationParams_String* ToastConfigData_String, ToastNotificationParams_Boolean* ToastConfigData_Boolean, ToastNotificationParams_Date* ToastConfigData_Date);
