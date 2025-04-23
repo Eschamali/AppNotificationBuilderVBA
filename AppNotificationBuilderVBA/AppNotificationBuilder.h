4e0337d2e27f24f74cca938b9e650d21610e6e14
Eschamali <mokkun75@gmail.com>
2025-04-23 12:34:32 +0900

    無駄な include を除去
    include 目的も追記

--- AppNotificationBuilderVBA/AppNotificationBuilder.h
+++ AppNotificationBuilderVBA/AppNotificationBuilder.h
@@ -1,25 +1,19 @@
-﻿#pragma once
+﻿#pragma once                                            //おまじない
 
-#include <windows.h>  // Windows APIの基本的な型や関数を含む
-
-#include <winrt/Windows.UI.Notifications.h>         //トースト関連モジュール全般
-#include <winrt/Windows.UI.Notifications.Management.h>
-#include <winrt/Windows.Data.Xml.Dom.h>             //XMLコンテンツ解析モジュール
-#include <winrt/Windows.Foundation.h>               //Nullable 系を扱うためのモジュール
-#include <winrt/Windows.Foundation.Collections.h>   //NotificationDataを扱うためのモジュール
-#include <winrt/base.h>                             //WinRT-APIの基本モジュール
-
-#include <string>                                   //文字列へ変換系モジュール
-#include <chrono>
-
-#include <comdef.h>
-#include <atlbase.h>
-#include <atlcom.h>
-#include <Exdisp.h>
-#include <Exdispid.h>
-#include <iostream>
+//必要なライブラリ等を読み込む
+#include <windows.h>                                    //Windows APIの基本的な型や関数を含む(今回は、Windows::Foundation::DateTime で使用します)
+#include <winrt/base.h>                                 //WinRT-APIの基本モジュール
+#include <winrt/Windows.UI.Notifications.h>             //トースト関連モジュール全般
+#include <winrt/Windows.UI.Notifications.Management.h>  //トースト関連モジュール制御
+#include <winrt/Windows.Data.Xml.Dom.h>                 //XMLコンテンツ解析モジュール
+#include <winrt/Windows.Foundation.h>                   //Nullable 系を扱うためのモジュール
+#include <winrt/Windows.Foundation.Collections.h>       //NotificationDataを扱うためのモジュール
+#include <atlbase.h>                                    //Excelインスタンス制御関連
+#include <comdef.h>                                     //デバッグによるエラーチェック用
 
 
+//外部参照設定つまりはVBAからでもアクセスできるようにする設定。おまじないと思ってください。
+//詳細→https://liclog.net/vba-dll-create-1/
 #ifdef AppNotificationBuilderVBA_EXPORTS
 #define AppNotificationBuilderVBA_API __declspec(dllexport)
 #else
